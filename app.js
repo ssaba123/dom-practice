9e364f340c7b931451464e79c40eca74808f50f6
sabashaikh1 <ss.sabashaikh1@gmail.com>
2022-10-27 14:35:21 -0400

    add styling

---
+++ app.js
@@ -0,0 +1,6 @@
+const container = document.querySelector('.container');
+const mseconds = document.querySelector('.timer__milliseconds')
+const start = document.querySelector('.start')
+
+let count = 0
+start.addEventListener('click', alert('he'))
